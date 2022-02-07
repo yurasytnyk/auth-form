3e5306d3c1a357673f22fb42b07b76557353c21e
Yura Sytnyk <yurii.sytnyk@spd-ukraine.com>
2022-02-07 11:02:17 +0200

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
-import { FC } from "react";
+import { FC } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { withRouteGuard } from './hoks/with-route-guard/hok';
+
+import { useAuth } from './hooks/useAuth/useAuth';
+import { LoginPage } from './pages/login-page/module';
+import { RegistrationPage } from './pages/registration-page/module';
+import { UserPage } from './pages/user-page/module';
+
+const Home = () => <div>Home page</div>;
+const ProtectedHome = withRouteGuard(Home);
 
 export const App: FC = () => {
-  return <div>app page</div>
+  const isAuth = useAuth();
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<UserPage auth={isAuth} />} />
+        <Route path="/home" element={<ProtectedHome auth={isAuth} />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+      </Routes>
+    </>
+  );
 };
