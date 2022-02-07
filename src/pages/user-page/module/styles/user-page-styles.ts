ede4e34d5dd4ac70b6bdc52c7291dac7e163fadf
Yura Sytnyk <yurii.sytnyk@spd-ukraine.com>
2022-02-07 10:54:41 +0200

    add user page

---
+++ src/pages/user-page/module/styles/user-page-styles.ts
@@ -0,0 +1,16 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useUserPageStyles = makeStyles({
+  cardWrapper: {
+    marginTop: '30px',
+    textAlign: 'center',
+  },
+  userAvatar: {
+    width: '90px',
+    height: '90px',
+    margin: '0 auto',
+  },
+  cardActions: {
+    justifyContent: 'center',
+  },
+});
